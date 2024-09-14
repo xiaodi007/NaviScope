ca61ad53eee37bb8f3c215b0cbf5f1db24b8cbc4
xiaodi007 <334830452@qq.com>
2024-09-14 16:11:09 +0800

    fix navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import Navbar from '../components/navbar';
+import Navbar from '../components/Navbar';
 
 const Home = () => {
   const { t } = useTranslation('home');
